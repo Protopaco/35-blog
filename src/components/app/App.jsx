bf277bded93385652ed1f33ebc801174d27e3b44
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-17 15:59:48 -0800

    try again

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PostList from '../posts/PostList.jsx';
-import PostForm from '../form/PostForm.jsx';
+import PostForm from '../form/PostForm';
 
 
 export default function App() {
