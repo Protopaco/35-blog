324d1280c87866675a9a38ac9c165dac1f37a39f
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-17 14:39:17 -0800

    adds reducer tests

---
+++ src/reducers/postReducer.test.js
@@ -0,0 +1,36 @@
+import { createPost, deletePost } from '../actions/postActions';
+import reducer from './postReducer';
+
+const newPost = {
+    title: 'hello',
+    body: 'world',
+    id: 5
+};
+
+describe('post reducer tests', () => {
+    it('add a post with CREATE_POST action', () => {
+        const state = {
+            posts: []
+        };
+
+        const action = createPost(newPost);
+        const newState = reducer(state, action);
+
+        expect(newState).toEqual({
+            posts: [newPost]
+        });
+    });
+
+    it('delete a post with DELETE_POST action', () => {
+        const state = {
+            posts: [newPost]
+        };
+
+        const action = deletePost(newPost.id);
+
+        expect(reducer(state, action)).toEqual({ posts: [] });
+    });
+
+});
+
+
